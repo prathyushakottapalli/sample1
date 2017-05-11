0a8e230e25f813a154d6b96a75d1ee0d6136daa1
prathyusha <prathyushakottapalli2115@gmail.com>
2017-05-11 11:39:48 +0530

    action

---
+++ 7.pl
@@ -0,0 +1,10 @@
+$string = "hi,am,prathyusha";
+@string =split(',',$string);
+print "@string\n";
+ foreach (@string)
+        {
+           print "$_\n";
+         }   
+print "$string[1]\n";
+$string_full = join(',',@string);
+print "$string\n";
